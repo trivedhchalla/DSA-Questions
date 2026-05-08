bce7f3e93a1c96bb9a213373df100b53d6c1e60e
Challa Trivedh Kumar <ctrivedh@gmail.com>
2026-05-08 23:58:41 +0530

    Add: combination-target-sum-ii - submission-3

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    Set<List<Integer>> ans = new HashSet<>(); 
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        backTrack(new ArrayList<>(), target, candidates, 0);
+        return new ArrayList<>(ans);
+    }
+
+    public void backTrack(List<Integer> subset, int sum, int[] nums, int index){
+        if(sum == 0){
+            ans.add(new ArrayList<>(subset));
+            return ;
+        }
+        if(sum < 0){
+            return ;
+        }
+        for(int i = index; i < nums.length; i++){
+            subset.add(nums[i]);
+            backTrack(subset, sum - nums[i], nums, i + 1);
+            subset.remove(subset.size() - 1);
+        }
+    }
+
+}
